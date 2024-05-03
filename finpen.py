801822ce1b56b9685671211c7736c1cdb1becc8f
aaryanpatell <aaryan6@outlook.com>
2024-05-03 14:15:31 -0600

    new changes

--- finpen.py
+++ finpen.py
@@ -1,8 +1,154 @@
-import requests
+from yahoo_fin.stock_info import get_data
+from copy import deepcopy as dc
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import numpy as np
+import torch
+import torch.nn as nn
 
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
-r = requests.get(url)
-data = r.json()
+class TimeSeriesDataset(Dataset):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __len__(self):
+        return len(self.x)
+    
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
 
-print(data)
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_stacked_layers):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_stacked_layers = num_stacked_layers
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_stacked_layers, 
+                            batch_first=True)
+        
+        self.fc = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        h0 = torch.zeros(self.num_stacked_layers, batch_size, self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_stacked_layers, batch_size, self.hidden_size).to(device)
+        
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+
+def prepare_dataframe(df, n_steps):
+    df = dc(df)
+
+    for i in range(1, n_steps + 1):
+        df[f'close(t-{i})'] = df['close'].shift(i)
+
+    df.dropna(inplace=True)
+
+    return df
+
+def train_one_epoch():
+    model.train(True)
+    print(f'Epoch: {epoch + 1}')
+    running_loss = 0.0
+    
+    for batch_index, batch in enumerate(train_loader):
+        x_batch, y_batch = batch[0].to(device), batch[1].to(device)
+        
+        output = model(x_batch)
+        loss = loss_function(output, y_batch)
+        running_loss += loss.item()
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch_index % 100 == 99:  # print every 100 batches
+            avg_loss_across_batches = running_loss / 100
+            print('Batch {0}, Loss: {1:.3f}'.format(batch_index + 1, avg_loss_across_batches))
+            running_loss = 0.0
+    print()
+
+def validate_one_epoch():
+    model.train(False)
+    running_loss = 0.0
+    
+    for batch_index, batch in enumerate(test_loader):
+        x_batch, y_batch = batch[0].to(device), batch[1].to(device)
+        
+        with torch.no_grad():
+            output = model(x_batch)
+            loss = loss_function(output, y_batch)
+            running_loss += loss.item()
+
+    avg_loss_across_batches = running_loss / len(test_loader)
+    
+    print('Val Loss: {0:.3f}'.format(avg_loss_across_batches))
+    print('***************************************************')
+    print()
+
+
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+print(device)
+
+amazon_data = get_data("amzn")
+amazon_data = amazon_data[['close']]
+
+st_length = 7
+data = prepare_dataframe(amazon_data, st_length)
+data = data.to_numpy()
+
+scaler = MinMaxScaler(feature_range=(-1,1))
+data = scaler.fit_transform(data)
+
+x = dc(np.flip(data[:, 1:], axis=1))
+y = data[:, 0]
+
+index_splitter = int(len(x) * 0.95)
+x_train = x[:index_splitter].reshape((-1, st_length, 1))
+x_test = x[index_splitter:].reshape((-1, st_length, 1))
+y_train = y[:index_splitter].reshape((-1, 1))
+y_test = y[index_splitter:].reshape((-1, 1))
+
+x_train = torch.tensor(x_train).float()
+x_test = torch.tensor(y_test).float()
+y_train = torch.tensor(y_train).float()
+y_test = torch.tensor(y_test).float()
+
+train_dataset = TimeSeriesDataset(x_train, y_train)
+test_dataset = TimeSeriesDataset(x_test, y_test)
+
+batch_size = 16
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+for _, batch in enumerate(train_loader):
+    x_batch, y_batch = batch[0].to(device), batch[1].to(device)
+    print(x_batch.shape, y_batch.shape)
+    break
+
+model = LSTM(1, 4, 1)
+model.to(device)
+
+learning_rate = 0.001
+num_epochs = 10
+loss_function = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    train_one_epoch()
+    # validate_one_epoch()
+
+with torch.no_grad():
+    predicted = model(x_train.to(device)).to('cpu').numpy()
+
+plt.plot(y_train, label='Actual Close')
+plt.plot(predicted, label='Predicted Close')
+plt.xlabel('Day')
+plt.ylabel('Close')
+plt.legend()
+plt.show()
